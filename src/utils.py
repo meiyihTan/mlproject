c6b751002ffca67cbbe6a57b5bd1f36a95775b43
meiyihTan <meiyihtan@gmail.com>
2024-04-20 18:29:55 +0800

    Hyperparameter Training

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import dill
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -19,15 +20,22 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def train_and_evaluate_models(X_train,y_train,X_test,y_test,models):
+def train_and_evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report={}
         
         for i in range(len(list(models))):
             # loop through each model
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-            model.fit(X_train, y_train) #Train model
+            #apply GridSearch or randomiseCV (to perform hyperparam tuning on the model)
+            gs= GridSearchCV(model,para,cv=3)# initialiase GridSearchCV object ; do 3-fold cross-validation
+            gs.fit(X_train,y_train) # fits the GridSearchCV to training data; Performs grid search to find the best hyperparam for the model using this train data. ( training of the model occurs for every combination of hyperparameters for 3 times(cv=3), and the model's performance is evaluated using cross-validation to determine the best hyperparameters.)
+
+            #select the best hyperparam and set them to the model
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train) #Train model with the best hyperparams obtained from the grid search.
 
             # Make predictions
             y_train_pred =model.predict(X_train)
